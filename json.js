a14135741b918430a27475c9424a6b1f848b3193
Kazi Md. Rakibul Hassan <2220936@iub.edu.bd>
2024-10-01 21:13:04 +0600

    stringify

--- json.js
+++ json.js
@@ -2,4 +2,5 @@ const user = {id: 1, name: 'John', job: 'Developer'};
 console.log(user);
 // Output: {id: 1, name: 'John', job: 'Developer'}
 
-JSON.stringify(user);
+const stringified = JSON.stringify(user);
+console.log(stringified);
