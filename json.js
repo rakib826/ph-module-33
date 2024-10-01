8d93990e1e41adf396bf7396ac5ccaa8f8f30d1b
Kazi Md. Rakibul Hassan <2220936@iub.edu.bd>
2024-10-01 21:57:53 +0600

    fetch basic

--- json.js
+++ json.js
@@ -4,3 +4,22 @@ console.log(user);
 
 const stringified = JSON.stringify(user);
 console.log(stringified);
+
+const shop = {
+  owner: 'John',
+  address: {
+    street:'13th Street Main',
+    city: 'New York',
+    country: 'USA'
+  },
+  products: ['TV', 'Fridge', 'Washing Machine'],
+  revenue: 1000000,
+  isOpen: true,
+  isClosed: false
+}
+console.log(shop);
+const shopJSON = JSON.stringify(shop);
+console.log(shopJSON);
+
+const shopObj = JSON.parse(shopJSON);
+console.log(shopObj);
